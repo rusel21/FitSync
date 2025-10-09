61e2a51324c1e3d39762549d4630a227b2518762
Marc Hael Uy <marchael28@gmail.com>
2025-10-09 17:36:01 +0800

    Add Membership, Payment, and Dash CSS + update components

--- Backend/Frontend/src/pages/admin/AdminDashboard.jsx
+++ Backend/Frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,7 +1,13 @@
+import Navbar from "../../components/navbar"
+import React from "react";
+
+
 export default function AdminDashboard(){
     return(
         <>
-        <h1>AdminDashboard</h1>
+        AdminDashBoard
         </>
     )
 }
+
+  
